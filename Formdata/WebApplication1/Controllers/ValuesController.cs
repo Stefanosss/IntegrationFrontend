7b7741268590e3eb1b952f496dff4c321a57fb36
Stefanosss <32611932+Stefanosss@users.noreply.github.com>
2018-04-26 23:29:03 +0200

    Add files via upload

---
+++ Formdata/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpPost("reserveren")]
+        public int Create([FromBody]Bezoeker b)
+        {
+            Console.WriteLine("separtiii");
+            var factory = new ConnectionFactory() { HostName = "10.3.50.48", Port = 5672, UserName = "radmin", Password = "radmin" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "rabbitmq", type: "fanout", durable: true);
+
+                var message = b.ToXML();
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "rabbitmq",
+                                     routingKey: "",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            return 1;
+
+        }
+    }
+    
+}
---
+++ Formdata/WebApplication1/Models/Bezoeker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Bezoeker
+    {
+        
+            public string firstname { get; set; }
+            public string lastname { get; set; }
+            public string address { get; set; }
+            public string email { get; set; }
+            public bool ontbijt { get; set; }
+            public bool factuur { get; set; }
+
+            public string ToXML()
+            {
+            return "<uuid>456564456</uuid>" +
+            "<type>bezoeker</type>" +
+            $"<firstname>{ this.firstname }</firstname>" +
+            $"<lastname>{ this.lastname }</lastname>" +
+            $"<address>{ this.address }</address>" +
+            $"<email>{ this.email }</email>" +
+            "<isActive>true</isActive>" +
+            "<isAllowed>true</isAllowed>" +
+            "<roles></roles>" +
+            "<version>1</version>" +
+            $"<timestamp>{ DateTime.Now }</timestamp>";
+            }
+        
+
+
+    }
+}
